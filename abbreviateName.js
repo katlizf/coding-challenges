73d2c17486501263d94f2b970d937a8bb8484c1a
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-05-31 09:58:40 -0500

    completed abbreviateName challeng

---
+++ abbreviateName.js
@@ -0,0 +1,16 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+function abbrevName(name){
+    let nameArr = name.split(' ')
+    let first = nameArr[0][0]
+    let last = nameArr[1][0]
+    return first.toUpperCase() + '.' + last.toUpperCase()
+}
